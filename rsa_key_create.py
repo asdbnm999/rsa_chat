4c9df97c19e149c5b95bffe56c83a33a85148159
asdbnm999 <alex.porubaev@mail.ru>
2024-10-02 12:50:10 +0300

    changes

--- rsa_key_create.py
+++ rsa_key_create.py
@@ -4,9 +4,9 @@ key = RSA.generate(2048)
 private_key = key.export_key()
 public_key = key.publickey().export_key()
 
-with open('private_key.pem', 'wb') as f:
+with open('password_private_key.pem', 'wb') as f:
     f.write(private_key)
 
-with open('public_key.pem', 'wb') as f:
+with open('password_public_key.pem', 'wb') as f:
     f.write(public_key)
 
